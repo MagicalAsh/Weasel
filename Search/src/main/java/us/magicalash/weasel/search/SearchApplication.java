09e992ad8846154b1ff3194c43b0ce9f81ea123d
Wesley R <wsrogers3@catamount.wcu.edu>
2019-11-13 20:42:29 -0500

    Removed a dummy method.

--- Search/src/main/java/us/magicalash/weasel/search/SearchApplication.java
+++ Search/src/main/java/us/magicalash/weasel/search/SearchApplication.java
@@ -1,11 +1,7 @@
 package us.magicalash.weasel.search;
 
-import com.google.gson.JsonObject;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.web.bind.annotation.CrossOrigin;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
@@ -16,9 +12,4 @@ public class SearchApplication {
         SpringApplication.run(SearchApplication.class, args);
     }
 
-    @CrossOrigin
-    @PostMapping(value = "/search", consumes = "*/*")
-    public JsonObject a(@RequestBody JsonObject obj){
-        return obj;
-    }
 }
