0c35c99c44d0983d7194dbb3313fd11d7a2fd21b
Wesley R <wsrogers3@catamount.wcu.edu>
2019-11-19 12:33:43 -0500

    Forgot to create a plugins folder for :CommonConfiguration tests.

--- installPlugins.sh
+++ installPlugins.sh
@@ -1,5 +1,9 @@
 mkdir plugins/
 
+# Create a dummy directory for use when testing the plugin loader
+mkdir ./CommonConfiguration/plugins
+
+# Create a link for the actual plugins to be available during testing
 cp ./Plugins/*/build/libs/* ./plugins
 ln -s ../plugins ./Index/plugins
 ln -s ../plugins ./Provider/plugins
