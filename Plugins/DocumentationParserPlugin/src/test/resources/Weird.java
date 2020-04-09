4b14b76c5f061ed766c90d209dd3c96cdac90341
Wesley R <wsrogers3@catamount.wcu.edu>
2020-04-09 00:41:54 -0400

    Forgot to commit an extra test file.

---
+++ Plugins/DocumentationParserPlugin/src/test/resources/Weird.java
@@ -0,0 +1,20 @@
+/**
+ * this is a package doc comment
+ */
+package foo.bar;
+
+/**
+ * foo
+ */
+
+/**
+ * bar
+ */
+public class Bar /** weird comment */ {
+
+    /** weird extra comment */
+    /** actual javadoc comment */
+    public void foo() {
+        /** weird */
+    }
+}
